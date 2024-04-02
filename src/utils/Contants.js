85d8f94e469586faa88be40d3b3ed6ec378d4495
darkshadows02 <148308065+darkshadows02@users.noreply.github.com>
2024-04-02 22:52:47 +0530

    first1 commit

---
+++ src/utils/Contants.js
@@ -0,0 +1,5 @@
+export const CDN_URL="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+
+export const LOGO_URL="https://is4-ssl.mzstatic.com/image/thumb/Purple113/v4/6a/ff/f8/6afff8a9-851a-b817-6c7f-8130cc8175b2/source/512x512bb.jpg";
+
+export const MENU_API="https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=23.2616084&lng=87.8491064&restaurantId=";
